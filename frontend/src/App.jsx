3cf2d4c82e3fba657ca5083977bf0a6cfc87e962
saba <marghanias@gmail.com>
2024-08-23 22:15:23 +0400

    created header and footer added styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
 const App = () => {
-	return <h1>Welcome to ProShop</h1>;
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to ProShop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
