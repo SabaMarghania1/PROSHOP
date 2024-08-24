c5c5cdffa0b99f63d789964a601cd8698f0bac32
saba <marghanias@gmail.com>
2024-08-24 14:29:56 +0400

    Added details page screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,11 +13,13 @@ import App from './App.jsx';
 import './assets/styles/index.css';
 
 import HomeScreen from './screens/HomeScreen.jsx';
+import ProductScreen from './screens/ProductScreen.jsx';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path='/' element={<App />}>
 			<Route index path='/' element={<HomeScreen />} />
+			<Route path='/product/:id' element={<ProductScreen />} />
 		</Route>
 	)
 );
