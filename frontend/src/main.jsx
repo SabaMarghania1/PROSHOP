cbece2ff83d083d5de6182267503056ec4c78bf4
saba <marghanias@gmail.com>
2024-08-30 00:41:06 +0400

    Private Route Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,16 +11,17 @@ import {
 import { Provider } from "react-redux";
 import store from "../store.js";
 
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
-
-import "./assets/styles/bootstrap.custom.css";
-import "./assets/styles/index.css";
 import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
+import PrivateRoute from "./Components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,7 +31,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
